a67ae3d578d21c696fb55108a39cf1d8ffc547a0
naatbarcelos <naathalia.b@hotmail.com>
2023-12-30 11:04:37 -0300

    java

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector('#profile img')
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+        
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
+    /*uma opcao, mas da pra ser mais simples
+    if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }*/
+
+}
